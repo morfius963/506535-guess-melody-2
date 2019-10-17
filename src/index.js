f3d68d4c8f83eb92d2ac5cf256c0880c83edfead
Vitalii <morfius963@gmail.com>
2019-10-17 22:43:22 +0300

    Добавил значения пропсам в компонент WelcomeScreen

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import WelcomeScreen from "./components/welcome-screen/welcome-scren.jsx";
 
+const MockData = {
+  GAME_TIME: 8,
+  ERROR_COUNT: 4
+};
+
 const init = () => {
   ReactDOM.render(
-      <WelcomeScreen />,
+      <WelcomeScreen
+        time = {MockData.GAME_TIME}
+        errorCount = {MockData.ERROR_COUNT}
+      />,
       document.querySelector(`#root`)
   );
 };
