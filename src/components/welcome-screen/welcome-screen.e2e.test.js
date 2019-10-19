c390a5be536f2ed9874b88e0b36d9f621af29e50
Vitalii <morfius963@gmail.com>
2019-10-19 15:04:17 +0300

    Написал e2e test

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WelcomeScreen from "./welcome-scren.jsx";
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe(`end to end test`, () => {
+  it(`Click event correctly works on start button`, () => {
+    const clickHandler = jest.fn();
+    const app = shallow(<WelcomeScreen
+      time = {0}
+      errorCount = {0}
+      onButtonClick = {clickHandler}
+    />);
+    const startButton = app.find(`.welcome__button`);
+
+    startButton.simulate(`click`);
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+  });
+});
