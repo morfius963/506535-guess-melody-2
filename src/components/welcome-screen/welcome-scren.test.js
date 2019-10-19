905026e008ba214f2ed0cdfe390c5454446c19ef
Vitalii <morfius963@gmail.com>
2019-10-19 15:03:53 +0300

    Обновил snapshot

--- src/components/welcome-screen/welcome-scren.test.js
+++ src/components/welcome-screen/welcome-scren.test.js
@@ -4,10 +4,12 @@ import WelcomeScreen from "./welcome-scren.jsx";
 
 describe(`snapshot test`, () => {
   it(`App correctly renders`, () => {
+    const clickHandler = jest.fn();
     const tree = renderer
       .create(<WelcomeScreen
         time = {0}
         errorCount = {0}
+        onButtonClick = {clickHandler}
       />)
       .toJSON();
     expect(tree).toMatchSnapshot();
