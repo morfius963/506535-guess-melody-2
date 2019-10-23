cdf4618039f73f8f12748c04348a5f8357e715eb
Vitalii <morfius963@gmail.com>
2019-10-23 21:54:26 +0300

    Изменил названия пропсов

--- src/components/welcome-screen/welcome-scren.jsx
+++ src/components/welcome-screen/welcome-scren.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const WelcomeScreen = ({time, errorCount, onButtonClick}) => {
+const WelcomeScreen = ({gameTime, errorCount, onButtonClick}) => {
   return <section className="welcome">
     <div className="welcome__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" /></div>
     <button className="welcome__button" onClick={onButtonClick}><span className="visually-hidden">Начать игру</span></button>
     <h2 className="welcome__rules-title">Правила игры</h2>
     <p className="welcome__text">Правила просты:</p>
     <ul className="welcome__rules-list">
-      <li>За {time} минут нужно ответить на все вопросы.</li>
+      <li>За {gameTime} минут нужно ответить на все вопросы.</li>
       <li>Можно допустить {errorCount} ошибки.</li>
     </ul>
     <p className="welcome__text">Удачи!</p>
@@ -16,7 +16,7 @@ const WelcomeScreen = ({time, errorCount, onButtonClick}) => {
 };
 
 WelcomeScreen.propTypes = {
-  time: PropTypes.number.isRequired,
+  gameTime: PropTypes.number.isRequired,
   errorCount: PropTypes.number.isRequired,
   onButtonClick: PropTypes.func.isRequired
 };
