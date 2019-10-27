f13bec1f8c72b3883d4aedb1c785a1ff830bc9e9
Vitalii <morfius963@gmail.com>
2019-10-27 23:43:33 +0200

    Обновил снепшот тесты

--- src/components/genre-question-screen/genre-question-screen.test.js
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -5,6 +5,10 @@ import {questions} from "../../mocks/questions.js";
 
 describe(`snapshot test`, () => {
   it(`Component correctly renders`, () => {
+    const mockNullComponent = jest.fn().mockReturnValue(null);
+    jest.mock(`../game-header/game-header.jsx`, () => mockNullComponent);
+    jest.mock(`../genre-answer/genre-answer.jsx`, () => mockNullComponent);
+
     const currentQuestion = questions.find((question) => question.type === `genre`);
     const clickHandler = jest.fn();
     const tree = renderer
