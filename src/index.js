576ddfb5f46b2a1e17f06bbf6fe19c89646632dd
Vitalii <morfius963@gmail.com>
2019-11-03 01:05:26 +0200

    Пропс времени теперь передаю через глобальное состояние приложения

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ import {settings, questions} from "./__fixtures__/questions.js";
 import {reducer} from "./reducer/reducer.js";
 
 const init = (gameQuestions) => {
-  const {errorCount, gameTime} = settings;
+  const {errorCount} = settings;
   const store = createStore(reducer);
 
   ReactDOM.render(
       <Provider store={store} >
         <App
-          time = {gameTime}
           maxMistakes = {errorCount}
           questions = {gameQuestions}
         />
