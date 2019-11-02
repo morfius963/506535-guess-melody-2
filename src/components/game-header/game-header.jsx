b823ba48cd3b11ecc7b3eddee11e8e422890cfa5
Vitalii <morfius963@gmail.com>
2019-11-03 01:04:20 +0200

    Вложенные компоненты теперь передаю не через children

--- src/components/game-header/game-header.jsx
+++ src/components/game-header/game-header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import propTypes from "./prop-types.js";
+import GameTime from "../game-time/game-time.jsx";
+import GameMistakes from "../game-mistakes/game-mistakes.jsx";
 
-const GameHeader = ({children}) => {
+const GameHeader = ({mistakes, maxMistakes, gameTime, onTimeUpdate, onTimeEnd}) => {
   return <header className="game__header">
     <a className="game__back" href="#">
       <span className="visually-hidden">Сыграть ещё раз</span>
@@ -12,13 +14,9 @@ const GameHeader = ({children}) => {
       <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
     </svg>
 
-    <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-      <span className="timer__mins">05</span>
-      <span className="timer__dots">:</span>
-      <span className="timer__secs">00</span>
-    </div>
+    <GameTime time={gameTime} mistakes={mistakes} maxMistakes={maxMistakes} onTimeUpdate={onTimeUpdate} onTimeEnd={onTimeEnd} />
 
-    {children}
+    <GameMistakes mistakes={mistakes} />
 
   </header>;
 };
