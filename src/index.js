1506dc4e6048e78eb1396ffc628c7a601dacc736
Vitalii <morfius963@gmail.com>
2019-11-07 22:42:36 +0200

    Написал настройку для ReduxDevTools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import {reducer} from "./reducer/reducer.js";
 
 const init = (gameQuestions) => {
   const {errorCount} = settings;
-  const store = createStore(reducer);
+  const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  );
 
   ReactDOM.render(
       <Provider store={store} >
