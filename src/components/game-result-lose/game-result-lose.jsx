31488e6827a7b791df2a677480d0d2778dcd1124
Vitalii <morfius963@gmail.com>
2019-11-24 22:42:08 +0200

    Добавил компонент Link для редиректа

--- src/components/game-result-lose/game-result-lose.jsx
+++ src/components/game-result-lose/game-result-lose.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
+import {Link} from "react-router-dom";
 import propTypes from "./prop-types.js";
 
 const GameResultLose = ({result, restartGame}) => {
@@ -8,19 +8,12 @@ const GameResultLose = ({result, restartGame}) => {
     ? `У вас закончились все попытки. Ничего, повезёт в следующий раз!`
     : `Время вышло! Вы не успели отгадать все мелодии`;
 
-  const history = useHistory();
-
-  const gameRestartHandler = () => {
-    restartGame();
-    history.push(`/`);
-  };
-
   return (
     <section className="result">
       <div className="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" /></div>
       <h2 className="result__title">Какая жалость!</h2>
       <p className="result__total result__total--fail">{loseMessage}</p>
-      <button className="replay" type="button" onClick={gameRestartHandler}>Попробовать ещё раз</button>
+      <Link to="/" className="replay" onClick={restartGame}>Попробовать ещё раз</Link>
     </section>
   );
 };
