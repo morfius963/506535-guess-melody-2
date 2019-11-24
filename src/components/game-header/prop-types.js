1b4cc70e20f1090f7eab47fb7d7fc7f343eee27a
Vitalii <morfius963@gmail.com>
2019-11-24 22:38:58 +0200

    Обновил пропсы

--- src/components/game-header/prop-types.js
+++ src/components/game-header/prop-types.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default {
   mistakes: PropTypes.number.isRequired,
-  gameTime: PropTypes.number.isRequired,
+  time: PropTypes.number.isRequired,
   onTimeUpdate: PropTypes.func.isRequired,
   onTimeEnd: PropTypes.func.isRequired,
   registrateTimer: PropTypes.func.isRequired,
