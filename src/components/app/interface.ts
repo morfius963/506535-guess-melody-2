ebf2e6ffd3683fccd2c8597631c39dc48f07e32d
Vitalii <morfius963@gmail.com>
2019-11-30 16:40:33 +0200

    Обновил интерфейс

--- src/components/app/interface.ts
+++ src/components/app/interface.ts
@@ -1,40 +1,14 @@
+import * as React from "react";
 import {QuestionArtist, QuestionGenre} from "../../types";
 
 type Question = QuestionArtist | QuestionGenre;
-type GameResult = `` | `win` | `lose-time` | `lose-mistakes`;
-type UserData = {
-  email: string,
-  password: string
-};
 
 export interface Props {
-  time: number,
-  timeForGame: number,
   questions: Question[],
   questionStep: number,
   mistakes: number,
-  maxMistakes: number,
-  points: number,
-  quickAnswerCount: number,
   isLoading: boolean,
-  isAuthorizationRequired: boolean,
-  gameResult: GameResult,
-
-  onWelcomeScreenClick: () => void,
-  onUserAnswer: (
-      userAnswer: string | boolean[],
-      question: Question,
-      mistakes: number,
-      maxMistakes: number,
-      currentQuestionIndex: number,
-      maxQuestionIndex: number,
-      answerTime: number
-  ) => void,
-  onTimeUpdate: () => void,
-  onTimeEnd: () => void,
+  renderScreen: (question : Question) => React.ReactElement,
   resetGame: () => void,
-  restartGame: () => void,
-  loadQuestions: () => void,
-  registrateTimer: (id: number) => void,
-  postUserLogin: (userData: UserData, pushPath: () => void) => void
+  loadQuestions: () => void
 };
